2776ada951a8b18f98b90c5c0056daf3e2490b3e
Luke Aslanian <50999201+lukeaslanian@users.noreply.github.com>
2024-11-29 01:32:26 +0000

    fixes to tf tests

--- python_service/test_tf.py
+++ python_service/test_tf.py
@@ -1,10 +1,14 @@
 import tensorflow as tf
 
-print('TensorFlow version:', tf.__version__)
-print('Devices:', tf.config.list_physical_devices())
+def test_tensorflow_version():
+    assert tf.__version__ is not None
 
-# Simple TensorFlow operation
-a = tf.constant(2)
-b = tf.constant(3)
-c = a + b
-print('Result of computation:', c.numpy())
+def test_list_physical_devices():
+    devices = tf.config.list_physical_devices()
+    assert len(devices) > 0
+
+def test_tensorflow_operation():
+    a = tf.constant(2)
+    b = tf.constant(3)
+    c = a + b
+    assert c.numpy() == 5

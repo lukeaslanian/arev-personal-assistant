27cc24d081b3760a10720eaf8e82bd66c68749c3
Luke Aslanian <50999201+lukeaslanian@users.noreply.github.com>
2024-11-29 01:41:47 +0000

    improved chat.py

--- python_service/chat.py
+++ python_service/chat.py
@@ -6,13 +6,19 @@ model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
 
 def chat(user_input, chat_history_ids=None):
     new_input_ids = tokenizer.encode(user_input + tokenizer.eos_token, return_tensors='pt')
+    attention_mask = torch.ones(new_input_ids.shape, dtype=torch.long)
 
-    bot_input_ids = torch.cat([chat_history_ids, new_input_ids], dim=-1) if chat_history_ids is not None else new_input_ids
+    if chat_history_ids is not None:
+        bot_input_ids = torch.cat([chat_history_ids, new_input_ids], dim=-1)
+        attention_mask = torch.cat([torch.ones(chat_history_ids.shape, dtype=torch.long), attention_mask], dim=-1)
+    else:
+        bot_input_ids = new_input_ids
 
     chat_history_ids = model.generate(
         bot_input_ids,
         max_length=1000,
-        pad_token_id=tokenizer.eos_token_id
+        pad_token_id=tokenizer.eos_token_id,
+        attention_mask=attention_mask
     )
 
     response = tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
